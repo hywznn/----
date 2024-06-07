a0c5bfab7eceaa6782ebf51c10991c8a64a03e65
hywznn <choihj0510@naver.com>
2024-06-07 13:43:49 +0900

    uvicorn install

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from entry import entry_router  # Import the entry router
-
+import uvicorn
 app = FastAPI()
 
 origins = ["*"]
